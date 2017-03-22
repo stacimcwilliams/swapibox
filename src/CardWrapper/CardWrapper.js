02b9185fba0bb7c68da5b4488a99ea6ebbfeec5f
Staci McWilliams <stacinicole86@gmail.com>
2017-03-22 17:58:26 -0600

    cards for planet and vehicle rendering on page, card wrapper functional

--- src/CardWrapper/CardWrapper.js
+++ src/CardWrapper/CardWrapper.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import './CardWrapper.css';
 import Card from '../Card/Card.js'
+import VehicleCard from '../VehicleCard/VehicleCard.js'
+import PlanetCard from '../PlanetCard/PlanetCard.js'
 
 
-const categoryCheck = (props) => {
-  console.log(props);
-  // return !props ? <div>Select a Category</div> : <Card />
+
+
+const planetMap = (data) => {
+  return data.map((planet, index) => {
+    return(<PlanetCard key={index}
+                      data={planet} />
+          )
+  })
 }
 
-const CardWrapper = (props) => {
+const vehiclesMap = (data) => {
+  return data.map((vehicle, index) => {
+   return (<VehicleCard key={index}
+                         data={ vehicle }/>
+          )
+ })
+ }
+
+const getCards = (data, category) => {
+  console.log(category);
+  if(category === "vehicles") {
+    return vehiclesMap(data)
+
+  } else if(category === "planets") {
+    return planetMap(data)
+  }
+}
+
+{/* // const categoryCheck = (props) =>
+//   console.log(props);
+//   // return !props ? <div>Select a Category</div> : <Card />
+// } */}
+
+const CardWrapper = ( { data, category } ) => {
   return (
     <div className="card-wrapper">
-       <Card {...props}/>
+      {getCards(data, category)}
     </div>
   )
 }
