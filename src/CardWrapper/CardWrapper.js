4855d0f08120cda370f47232800b45b711ac6337
Tim <tbrandle53@gmail.com>
2017-03-22 19:34:13 -0600

    getting closer

--- src/CardWrapper/CardWrapper.js
+++ src/CardWrapper/CardWrapper.js
@@ -1,47 +1,66 @@
 import React from 'react';
 import './CardWrapper.css';
-import Card from '../Card/Card.js'
+// import Card from '../Card/Card.js'
 import VehicleCard from '../VehicleCard/VehicleCard.js'
 import PlanetCard from '../PlanetCard/PlanetCard.js'
+import PeopleCard from '../PeopleCard/PeopleCard.js'
 
 
-
-
-const planetMap = (data) => {
-  return data.map((planet, index) => {
-    return(<PlanetCard key={index}
-                      data={planet} />
-          )
-  })
+const fetchPlanetApi = (obj) => {
+  let planetObject = {}
+  let { name } = obj;
+  fetch(obj.homeworld)
+      .then(data => data.json())
+      .then(json => {
+          let { population } = json
+          let planetName = json['name']
+          const personObj = { name, population, planetName }
+          Object.assign(planetObject, personObj)
+        })
+  return planetObject;
 }
 
-const vehiclesMap = (data) => {
-  return data.map((vehicle, index) => {
-   return (<VehicleCard key={index}
-                         data={ vehicle }/>
-          )
- })
- }
+const fetchSpeciesApi = (array) => {
+  const url = array[0]
+  let speciesObj = {}
+  fetch(url)
+      .then(data => data.json())
+      .then(json => {
+        let { name } = json
+        let nameObj = { 'species': name }
+        Object.assign(speciesObj, nameObj)
+      })
+  return speciesObj;
+}
 
-const getCards = (data, category) => {
-  console.log(category);
-  if(category === "vehicles") {
-    return vehiclesMap(data)
+const getStateObject = (data) => {
+  let temp = []
+  data.map(person => {
+    const planetResult = fetchPlanetApi(person)
+    const speciesResult = fetchSpeciesApi(person.species)
+    temp.push({planetResult, speciesResult})
+  });
+  return temp
+}
 
-  } else if(category === "planets") {
-    return planetMap(data)
+
+const cardGenerator = (data, category) => {
+  if (category === "planets") {
+    return data.map((category, index) => <PlanetCard key={index} data={category} />)
+   } else if (category === "vehicles") {
+     return data.map((vehicle, index) => <VehicleCard key={index} data={ vehicle }/>)
+   } else if (category === "people") {
+      const stateObject = getStateObject(data)
+      return stateObject.map((person, index) => <PeopleCard key={index} data={ person }/>)
   }
 }
 
-{/* // const categoryCheck = (props) =>
-//   console.log(props);
-//   // return !props ? <div>Select a Category</div> : <Card />
-// } */}
 
 const CardWrapper = ( { data, category } ) => {
+  // debugger
   return (
     <div className="card-wrapper">
-      {getCards(data, category)}
+      { cardGenerator(data, category) }
     </div>
   )
 }
